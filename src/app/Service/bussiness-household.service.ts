b49675e4826469cc7c5dfd27deec5ad0c84ce2ee
alantran99 <tranlevuluan@gmail.com>
2021-07-25 15:27:57 +0700

    Done Danh sách hộ kinh doanh Ver2
    Done

--- src/app/Service/bussiness-household.service.ts
+++ src/app/Service/bussiness-household.service.ts
@@ -55,14 +55,14 @@ export class BussinessHouseholdService {
   }
 
   public getForm(name, certificationNumber, createdDate, status): Observable<any>{
-    const url = this.REST_API_SERVER+'/search/'+name+'&'+certificationNumber+'&'+createdDate+'&'+status;
+    const url = this.REST_API_SERVER+'/search/'+status+'/'+name+'&'+certificationNumber+'&'+createdDate+'&'+status;
     return this.httpClient
     .get<any>(url,this.httpOptions)
     .pipe(catchError(this.handleError));
   }
 
   public getForms(name, certificationNumber, createdDate): Observable<any>{
-    const url = this.REST_API_SERVER+'/search_/'+name+'&'+certificationNumber+'&'+createdDate;
+    const url = this.REST_API_SERVER+'/search/'+name+'&'+certificationNumber+'&'+createdDate;
     return this.httpClient
     .get<any>(url,this.httpOptions)
     .pipe(catchError(this.handleError));
